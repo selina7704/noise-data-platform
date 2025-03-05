4a840b0dc320bb787e481d8cd76cc8ff56c4dbdb
EunSeo35 <pes1365@naver.com>
2025-03-05 15:31:50 +0900

    ES/resnet 모델 테스트 (파일 업로드)

--- web/streamlit_app.py
+++ web/streamlit_app.py
@@ -12,11 +12,13 @@ def main():
 
     if uploaded_file is not None:
         st.audio(uploaded_file, format='audio/wav')  # 업로드한 파일을 웹에서 들을 수 있게 표시
+        st.write(f"파일 이름: {uploaded_file.name}")
 
         # 예측 버튼 클릭 시 예측 수행
         if st.button('예측하기'):
             # 파일을 FastAPI 서버로 전송
-            response = requests.post(FASTAPI_URL, files={"file": uploaded_file.getvalue()})
+            files = {"file": (uploaded_file.name, uploaded_file.getvalue(), "audio/wav")}
+            response = requests.post(FASTAPI_URL, files=files)
 
             if response.status_code == 200:
                 prediction = response.json().get("prediction")
